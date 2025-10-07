2564191e882927980dad088e76580f377efebb2f
PhanPhucHau <phanphuchau15@gmail.com>
2025-10-07 14:26:12 +0700

    Add project files.

---
+++ ChairKitUI/Models/Account/Request/LoginRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChairKitUI.Models.Account.Request;
+
+public class LoginRequest
+{
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Vui lòng nhập email")]
+    public string Email { get; set; }
+
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Vui lòng nhập mật khẩu")]
+    [StringLength(maximumLength: 16, MinimumLength = 3, ErrorMessage = "Mật khẩu từ 3 đến 16 ký tự")]
+    public string Password { get; set; }
+    public bool RememberLogin { get; set; } = true;
+}
