96b458da403ebc7a84ba07d53c42b223ee1a24fc
PhanPhucHau <phanphuchau15@gmail.com>
2025-10-07 15:48:56 +0700

    update code Ui

---
+++ ChairKitUI/Components/Themes/CustomTheme.cs
@@ -0,0 +1,29 @@
+﻿using MudBlazor;
+
+namespace ChairKitUI.Components.Themes;
+
+public class CustomTheme
+{
+    public PaletteLight LightPalette { get; } = new()
+    {
+        Surface = "#d8d8d869",
+    };
+
+    public PaletteDark DarkPalette { get; } = new();
+
+    public MudTheme GetLightTheme()
+    {
+        return new MudTheme
+        {
+            PaletteLight = LightPalette
+        };
+    }
+
+    public MudTheme GetDarkTheme()
+    {
+        return new MudTheme
+        {
+            PaletteDark = DarkPalette
+        };
+    }
+}
